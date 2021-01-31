383ee8370ce85debe21ba8393c0365ea1de5bea3
foodytaing <foody.taing@gmail.com>
2021-01-31 04:01:16 +0100

    add background color pokemon style

--- src/components/PokemonInsert/index.js
+++ src/components/PokemonInsert/index.js
@@ -5,18 +5,22 @@ const Index = (props) => {
   const { data } = props;
   const [pkm, setPkm] = useState({});
 
+  const url = "https://pokeapi.co/api/v2/pokemon/" + data.name;
+
   useEffect(() => {
     axios
-      .get(data.url)
+      .get(url)
       .then((res) => {
         setPkm(res.data);
       })
       .catch((err) => console.log(err));
   }, []);
 
+  const pkmTypeClass = pkm?.types[0]?.type?.name || "";
+
   return (
     <>
-      <div className="pokemon-insert">
+      <div className={`pokemon-insert_${pkmTypeClass} pokemon-insert`}>
         <span className="pokemon-insert__name">{pkm?.name}</span>
         <img
           className="pokemon-insert__image"
